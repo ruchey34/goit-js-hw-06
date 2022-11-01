4201bb91b0aaa9a0e61c3e1893fa1a2732a9618d
ruchey34 <me.oleg1@gmail.com>
2022-11-01 11:59:01 +0200

    hotfix 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,9 @@
 const input = document.querySelector('#validation-input');
 
 input.addEventListener('blur', (event) => {
-    if(event.currentTarget.value.length === input.dataset.length)
+    console.log(event.currentTarget.value.length);
+    console.log(input.dataset.length);
+    if(event.currentTarget.value.length <= input.dataset.length)
     {
         input.classList.add("valid");
         input.classList.remove("invalid");
